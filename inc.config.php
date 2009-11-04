732b02e269282cce0d160418a8b3fea89881d4c1
Joe Crawford <joe@artlung.com>
2009-11-03 22:31:24 -0800

    config file

---
+++ inc.config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('CONSTANT_HOSTNAME', 'hostname');     // the adress of the MySQL server
+define('CONSTANT_USERNAME', 'username');    // your username
+define('CONSTANT_PASSWORD', 'password');     //your password
+define('CONSTANT_DATABASE', 'database-name');   // name of the database
+
+?>
